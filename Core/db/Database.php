cecd3d69b3ed7b3c56f3526149befd97ee502f86
Nikita <rngfdrknss@gmail.com>
2021-08-27 11:30:23 +0300

    Fix  Database.php.

--- Core/db/Database.php
+++ Core/db/Database.php
@@ -10,13 +10,14 @@ class Database
 
     public function __construct(array $config) 
     {   
-        $dsn = $config['dsn'];
-        $db_user = $config['db_user'];
-        $db_password = $config['db_password'];
+        $dsn = $config['dsn'] ?? '';
+        $db_user = $config['db_user'] ?? '';
+        $db_password = $config['db_password'] ?? '';
 
         //connection to db
         try {
             $this->pdo = new \PDO($dsn, $db_user, $db_password);
+            $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Error: ' . $e->getMessage();
         }
