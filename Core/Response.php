85f244f8d7c20c6c5d98929bd0dac5b8c193b89c
Nikita <rngfdrknss@gmail.com>
2021-08-27 10:42:54 +0300

    Create Core/Response.php

---
+++ Core/Response.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\Core;
+
+/*
+Класс отвечает за ответ http или сервера.
+
+Метод setstatuscode - устанавливает код ответа http
+Метод redirect - Устанавливает заголовок http, по сути перенаправляет по полученному маршруту
+*/
+
+class Response
+{
+    public function setStatusCode($code) 
+    {
+        http_response_code($code);
+    }
+
+    public function redirect($url)
+    {
+        header('Location: ' . $url);
+    }
+}
+
+?>
