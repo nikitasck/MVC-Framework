f98bbc0185d088c61ce181fa995c4e92d75cf7ce
Nikita <rngfdrknss@gmail.com>
2021-08-29 19:05:41 +0300

    Implemented session logic

---
+++ Core/Session.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\Core;
+
+class Session
+{
+   protected const FLASH_KEY = 'flash_messages';
+
+    public function __construct()
+    {
+        session_start();
+
+        //Регистрируем переменные в сессии. Устанавливаем ключ для удаления.
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+        foreach ($this->flashMessages as $key => $flashMessage) {
+            $flashMessage['remove'] = true;
+        }
+        
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
+    }
+
+    public function setFlash($key, $message)
+    {
+        $_SESSION[self::FLASH_KEY][$key] = [
+            'remove' => false,
+            'value' => $message
+        ];
+    }
+
+    public function getFlash($key)
+    {
+        return $_SESSION[self::FLASH_KEY][$key]['value'] ?? false;
+    }
+
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get($key)
+    {
+        return $_SESSION[$key];
+    }
+
+    public function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    public function __destruct()
+    {
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+        foreach($this->flashMessages as $key => $flashMessage) {
+            if($flashMessage['remove'] === true) {
+                unset($flashMessages[$key]);
+            }
+        }
+
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
+    }
+}
+
+?>
