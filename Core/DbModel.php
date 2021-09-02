2c67119f59afcd052ddfc5b6a016875a87806e24
Nikita <rngfdrknss@gmail.com>
2021-09-02 15:41:31 +0300

    Added update method to dbModel

--- Core/DbModel.php
+++ Core/DbModel.php
@@ -60,6 +60,31 @@ abstract class DbModel extends Model
         return $statement->fetchObject(static::class);
     }
 
+    /*
+    Метод для обновления данных в таблице
+    */
+
+    //$values = ['name' => 'user', 'email' => 'test@kek']
+    public function update($values, $where)
+    {
+        $rowId = $where;
+        $tableName = $this->tableName();
+        $params = array_keys($values);
+
+        $columnParam = implode('AND', array_map(fn($attr) => "$attr = :$attr", $params));
+
+        $sql = "UPDATE $tableName SET $columnParam WHERE $rowId";
+
+        $statement = self::prepare($sql);
+
+        foreach($values as $key => $value){
+            $statement->bindParam(":$key", $value);
+        }
+
+        $statement->execute();
+    }
+
+    //Подготовка запроса
     public static function prepare($sql)
     {
         return Application::$app->db->pdo->prepare($sql);
